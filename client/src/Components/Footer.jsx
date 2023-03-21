89970650b99404c0adf9dc3b1a633cba27f1c343
Tokyo <nezexmain@gmail.com>
2023-03-21 01:19:59 +0100

    Finaly

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Box, Container, Img, Text } from "@chakra-ui/react";
+import Logo from "../img/logoPng.png";
+const Footer = () => {
+  return (
+    <Box textAlign={"center"}>
+      <Container maxWidth={"container.lg"} color="black">
+        <Box
+          display={{ base: "block", lg: "flex" }}
+          mt={100}
+          mb={20}
+          justifyContent={"space-between"}
+        >
+          <hr style={{ border: "1px solid lightgray" }} />
+          <Box flex={5}>
+            <Img
+              margin={"0 auto"}
+              maxHeight={250}
+              maxWidth={400}
+              width={"100%"}
+              height={"100%"}
+              src={Logo}
+            />
+          </Box>
+
+          <Box flex={6} fontSize={20} mt={10}>
+            <Text fontWeight={"bold"}> Our main office:</Text>
+            <Text> PARIS, FRANCE E-Mail: info@victoriasoprano.com</Text>
+            <Text fontWeight={"bold"}>
+              Customer service department in Poland l.:
+            </Text>
+            <Text>+38 099 304 40 45 E-Mail: europa@victoriasoprano.com</Text>
+
+            <Text fontWeight={"bold"}>
+              {" "}
+              Customer service department in Ukraine,Russia el.:
+            </Text>
+            <Text>
+              {" "}
+              +38 067 383 50 77 l.: +38 093 630 14 14 EMail:
+              victoria.soprano@mail.ru
+            </Text>
+            <Text fontWeight={"bold"}>
+              {" "}
+              Customer service department in Canada, USA
+            </Text>
+            <Text> EMail: canada@victoriasoprano.com victoriasoprano.com</Text>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- client/src/Hook/useFetch.js
+++ client/src/Hook/useFetch.js
@@ -1,30 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export const useFetch = (URL) => {
   const [data, setData] = useState(null);
-  const [errors, setErrors] = useState(null);
+  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await axios.get(`http://localhost:1337/api/${URL}`, {
+        const response = await axios.get(`http://192.168.3.100:1337/api/${URL}`, {
           headers: { Authorization: `Bearer 5f3f44d009934699d30d17b924ae77894f273e306c4448a2752c1230fafbcaa6820d38c49ada5bbcd48d2909950c0fa07a7b5c65e17eab248acac2c5236a5752f7f724fa9dc64e7985da8cf1f03eac6e3a37edf9d2575cdec287745e4eb773ec2e8067c26e60ddf414efa51f7ec7bd84b75252bf39ea4b5c1da4f7f54300364b` },
         });
         setData(response.data);
         setIsLoading(false);
 
       } catch (error) {
-        setErrors(error);
+        setError(error);
+        navigate('/');
         setIsLoading(false);
       }
     };
     fetchData();
-    console.log(data);
   }, [URL]);
 
-  return { data, errors, isLoading };
+  return { data, error, isLoading };
 };
